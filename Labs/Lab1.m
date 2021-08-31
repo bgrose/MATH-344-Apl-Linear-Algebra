aef09a3eeb59bb033e974daa8cbbab9d1ebe047a
bgrose <47512745+bgrose@users.noreply.github.com>
2021-08-31 12:28:03 -0400

    lab1 edit

--- Labs/Lab1.m
+++ Labs/Lab1.m
@@ -2,15 +2,16 @@
 
 625^(3/4)
 
-sin(pi/4)
+% sin(pi/4)
 
 log10(exp(1))
 
 x=linspace(-10,10,200);
 
-f = (x.^3)/3-7;
+f = (x.^2)/3-7;
 
 g = x.*sin(x.^2);
 
-h = (x./(1+sqrt(abs(x))));                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+h = (x./(1+sqrt(abs(x))));              
+
 plot(x, f, x, g, x, h)

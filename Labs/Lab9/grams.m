c21739993ac67c84092b91e3d009fdb08ba4bc97
Bradley Grose <bradley@grose.net>
2021-10-25 08:27:04 -0400

    Update grams.m

--- Labs/Lab9/grams.m
+++ Labs/Lab9/grams.m
@@ -4,16 +4,17 @@
 % resulting form
 
 function Q = grams(A)
-    [~, n] = size(A);
-    Q = A;
+    [~, n] = size(A); % Get size
+    Q = A % Copy A to Q for Operations
     for i = 2:n
-        Q(:,i) = A(:,i);
+        Q(:,i) = A(:,i); %Sets first part of equation
         for j = 1:i-1
-            Q(:,i) = Q(:,i)-(Q(:,j)'*A(:,i))/(Q(:,j)'*Q(:,j)) * Q(:,j);
+            Q(:,i) = Q(:,i)-(Q(:,j)'*A(:,i))/(Q(:,j)'*Q(:,j)) * Q(:,j); %subtracts operation for j times based on i
         end
     end
     
-    for i = i:n
+    % Normalize Q
+    for i = 1:n
         Q(:,i) = Q(:, i)/norm(Q(:,i));
     end
 end
